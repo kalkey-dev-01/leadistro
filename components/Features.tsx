136602afd218b6946002567869a70e45b031eb72
MrJ0bs <ramanhalder12@gmail.com>
2022-10-20 19:03:13 +0530

    -features-need to complete

--- components/Features.tsx
+++ components/Features.tsx
@@ -2,6 +2,7 @@ import { useInView } from 'framer-motion';
 import * as React from 'react';
 import { BufferGeometry } from 'three';
 import { StyledCanvas } from './misc/Canvas';
+import { RButton, RoundedButton } from './misc/RoundedButton';
 
 
 interface FeatureProps {
@@ -14,7 +15,14 @@ export const Features: React.FC<FeatureProps> = ({ }) => {
 
     return (
         <>
-            <div className='bg-white min-h-screen text-black flex flex-col space-y-4'>
+            <div className='bg-white min-h-screen text-black flex  flex-col items-center '>
+                <RoundedButton>
+                    Features
+                </RoundedButton>
+                <div className="text-3xl md:text-6xl text-center">With all features</div>
+                <div className="text-3xl md:text-6xl text-center">you need</div>
+                {/* Cards */}
+                
             
 
 
--- components/misc/RoundedButton.tsx
+++ components/misc/RoundedButton.tsx
@@ -2,21 +2,98 @@ import React from 'react'
 import { MeshDistortMaterial, MeshWobbleMaterial, RoundedBox } from '@react-three/drei'
 import { MeshProps, ThreeEvent } from '@react-three/fiber'
 import { StyledCanvas } from './Canvas'
-import { motion } from 'framer-motion'
+import { animate, motion, MotionProps, useMotionValue, useTransform } from 'framer-motion'
 
 
 interface RBProps {
     children: React.ReactNode
-    className?: string
+    className?: string,
+    props?: MotionProps,
+    refEl?: any,
+    event?: React.MouseEvent
 }
 
 
 export const RoundedButton: React.FC<RBProps> = ({ children, className }) => {
 
+    function handleMouse(e: React.MouseEvent) {
+        console.log('Page x ->', e.pageX);
+        console.log('Page y ->', e.pageY);
+        console.log('Client x ->', e.clientX);
+        console.log('Client y ->', e.clientY);
+    }
+
+    return (
+        <motion.div whileHover={{ scale: 1.15 }} whileTap={{ scale: 0.55 }} className='cursor-pointer font-extrabold px-6 py-2 my-4 text-lg md:text-xl rounded-full border-black border-[1.5px]'>
+            <motion.div whileHover={{ scale: 1 }} whileTap={{ scale: 0.95 }}> {children}</motion.div>
+        </motion.div>
+    )
+}
+
+
+export const RButton: React.FC<RBProps> = ({ children, className, props }) => {
+    function getRelCoord(event: React.MouseEvent, refEl: HTMLDivElement) {
+        const pos = {
+            x: event?.pageX,
+            y: event?.pageY
+        }
+        const offset = {
+            left: refEl.offsetLeft,
+            top: refEl.offsetTop,
+            width: refEl.offsetWidth,
+            height: refEl.offsetHeight
+        }
+        let reference = refEl.offsetParent
+        while (reference instanceof HTMLDivElement) {
+            offset.left += reference.offsetLeft;
+            offset.top += reference.offsetTop;
+            reference = reference.offsetParent;
+        }
+        return {
+            x: pos.x - offset.left,
+            y: pos.y - offset.top,
+            width: offset.width,
+            height: offset.height,
+            centerX: (pos.x - offset.left - offset.width / 2) / (offset.width / 2),
+            centerY: (pos.y - offset.left - offset.width / 2) / (offset.width / 2)
+        }
+
+    }
+    const [mousePosition, setMousePosition] = React.useState<any>({});
+    const boxRef = React.useRef<HTMLDivElement>();
+    const handleMouse = (e: React.MouseEvent) => {
+        console.log('Page x ->', e.pageX);
+        console.log('Page y ->', e.pageY);
+        console.log('Client x ->', e.clientX);
+        console.log('Client y ->', e.clientY);
+
+        setMousePosition(getRelCoord(e, boxRef.current as HTMLDivElement))
+    }
     return (
         <>
-            <motion.div whileHover={{ scale: 1.25 }} className={`text-lg font-semibold cursor-pointer opacity-[0.85] rounded-3xl hover:opacity-100 border-[2.5px] border-white px-[12.5px] py-[1.5px] ${className}`}>
-                {children}
+            <motion.div animate={{
+                rotateX: mousePosition.centerX * 50,
+                rotateY: mousePosition.centerY * 100
+            }} onMouseMove={(e) => handleMouse(e)} style={{
+                width: "80px",
+                height: "30px",
+                position: "relative"
+            }} {...props}>
+                <motion.div style={{
+                    position: "absolute",
+                    width: "70px",
+                    height: "25px",
+                    margin: "-10px",
+                    borderRadius: 25,
+                    backgroundColor: 'aquamarine'
+                }}
+                    animate={{
+                        x: mousePosition.x,
+                        y: mousePosition.y
+                    }}
+                >
+                    {children}
+                </motion.div>
             </motion.div>
         </>
     )
